87deee886d0e3ffdc089da9d52cfa235276b4b29
deardrops <lness@qq.com>
2017-10-05 15:14:12 +0800

    fetch beatmap info && add nprogress component

--- index.js
+++ index.js
@@ -1,30 +1,61 @@
 function ppyUrl(sid) {
-  return 'https:/osu.ppy.sh/d/' + sid
+  return 'https:/osu.ppy.sh/d/' + sid;
 }
 function bloodcatUrl(sid) {
-  return 'http://bloodcat.osupink.org/d/' + sid
+  return 'http://bloodcat.osupink.org/d/' + sid;
 }
 
-// fetch response example:
+// beatmap api response example:
+// [{
+//   bmid: 953586,
+//   bmdid: 443751,
+//   artist: "Chino(CV.Minase Inori)",
+//   title: "Shinsaku no Shiawase wa Kochira!",
+//   version: "Happy~!",
+// }]
+
+function fetchBeatmapInfo(bid) {
+  return fetch('https://www.osupink.org/api/get_bm.php?b=' + bid, {
+    method: 'GET',
+  }).then(function(res){
+    if (!res.ok) throw new Error('error in fetch beatmap');
+    return res.json()
+  }).then(function(beatmaps){
+    if (!beatmaps.length) throw new Error('no beatmap: ' + bid);
+    return beatmaps[0];
+  });
+}
+
+// records api response example:
 // [{
 //   bid: 953586,
-//   sid: 443751,
-//   title: 'Chino(CV.Minase Inori) - Shinsaku no Shiawase wa Kochira!',
-//   diff: 'Happy~!',
 //   mods: 'HD HR',
 //   pp: 243,
 //   rank: 'A',
-// },{...}],
+// },{...},...],
+
+function fetchRecords(name, limit, mode) {
+  limit = limit ? limit : 20;
+  mode = mode ? mode : 0;
+  var query = '?user=' + name +
+              '&limit=' + limit +
+              '&mode=' + mode;
+  return fetch('https://www.osupink.org/api/get_bp.php' + query, {
+    method: 'GET',
+  }).then(function(res){
+    if (!res.ok) throw new Error('error in fetch records');
+    return res.json();
+  }).then(function(scores){
+    if (!scores.length) throw new Error('no records: ' + name);
+    return scores;
+  });
+}
 
 var app = new Vue({
   el: '#app',
   data: {
     tips: '',
-    loading: false,
 
-    i: 1,
-    animatedString: '',
-    
     name: '',
     limit: 20,
     mode: 0,
@@ -32,62 +63,69 @@ var app = new Vue({
     records: [],
   },
   computed: {
-
+    sortedRecords: function() {
+      return this.records.sort(function(a,b){
+        if (a.pp < b.pp) return 1;
+        if (a.pp > b.pp) return -1;
+        return 0;
+      });
+    },
   },
   methods: {
     submit: function() {
-      this.loading = true;
       this.records = [];
+      var vue = this;
 
-      var self = this;
-      var args = '?user=' + this.name +
-                 '&limit=' + this.limit +
-                 '&mode=' + this.mode;
-      fetch('https://www.osupink.org/api/get_bp.php' + args, {
-        method: 'GET',
-      }).then(function(res){
-        return res.json();
-      }).then(function(scores){
-        if (!scores.length) {
-          self.tips = 'no records, please check the username.';
-        }
-        scores.forEach(function(score) {
-          var record = {
-            bid: score.beatmap_id,
-            sid: 443751, // TODO
-            title: 'the truth that you leave', // TODO
-            diff: 'piano', // TODO
-            mods: score.mods,
-            pp: parseInt(score.pp),
-            rank: score.rank,
-          };
-          self.records.push(record);
+      NProgress.start();
+      fetchRecords(this.name,this.limit,this.mode)
+      .then(function(scores){
+        scores.forEach(function(score){
+          var bid = score.beatmap_id;
+          fetchBeatmapInfo(bid)
+          .then(function(beatmap){
+            var record = {
+              bid,
+              sid: beatmap.bmdid,
+              artist: beatmap.artist,
+              title: beatmap.title,
+              diff: beatmap.version,
+              mods: score.mods,
+              pp: parseInt(score.pp),
+              rank: score.rank,
+            };
+            vue.records.push(record);
+            NProgress.set(vue.records.length / scores.length);
+            if (vue.records.length === scores.length) {
+              this.recordsLoaded = true;
+            }
+          });
         });
-        self.loading = false;
-      }).catch(function(error) {
-        self.loading = false;
+      }).catch(function(error){
         console.log(error);
-        if (!res.ok) {
-          self.tips = 'serve error, please try again later.';
-        }
-      });
+        NProgress.done();
+      });;
+      // vue.showTips('no records, please check the username.');
+      // vue.showTips('serve error, please try again later.');
     },
-    sliceNext: function() {
-      this.animatedString = '......'.slice(-(this.i++ % 6));
+    showTips: function(tips) {
+      this.tips = tips;
+    },
+    clearTips: function() {
+      this.tips = '';
     },
     beatmapUrl: function(bid) {
-      return 'https://osu.ppy.sh/b/' + bid
+      return 'https://osu.ppy.sh/b/' + bid;
     },
     ppyUrl: function(bid) {
-      return ppyUrl(bid)
+      return ppyUrl(bid);
     },
     bloodcatUrl: function(bid) {
-      return bloodcatUrl(bid)
+      return bloodcatUrl(bid);
     },
     rankImgUrl: function(rank) {
-      return 'https://s.ppy.sh/images/' + rank + '_small.png'
+      return 'https://s.ppy.sh/images/' + rank + '_small.png';
     },
-    downloadAll: function(name) {
+    downloadAll: function() {
       this.records.forEach(function(record) {
         var iframe = document.createElement('iframe');
         iframe.src = ppyUrl(record.sid);
@@ -97,7 +135,6 @@ var app = new Vue({
     },
   },
   mounted: function() {
-    this.tips = 'Type username and query BPs.';
-    window.setInterval(this.sliceNext.bind(this), 500);
+    // this.showTips('Type username and query BPs.');
   },
 });
